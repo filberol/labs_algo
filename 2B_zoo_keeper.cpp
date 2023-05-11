b8fd82d327d7ccc49d860bdb29d0e0422cd35055
filberol <mkh.vadim@gmail.com>
2023-05-11 13:44:15 +0300

    Remove old

--- 2B_zoo_keeper.cpp
+++ 2B_zoo_keeper.cpp
@@ -2,54 +2,6 @@
 #include <stack>
 using namespace std;
 
-//int main() {
-//    string positions;
-//    vector<char> alphabet;
-//    // Read and transform string
-//    cin >> positions;
-//    if (positions.length() > 100000) return 0;
-//    vector<char> pos;
-//    for (char position: positions) {
-//        if (isupper(position)) {
-//            //cout << "Pushed " << position << "\n";
-//            alphabet.push_back(tolower(position));
-//        }
-//        pos.push_back(tolower(position));
-//    }
-//    //cout << string(alphabet.begin(), alphabet.end()) << "\n";
-//
-//    size_t oldLength = 100001;
-//    size_t length = positions.length();
-//    vector<int> solution;
-//    while (length < oldLength && length > 0) {
-//        oldLength = length;
-//        for (int i = 0; i < pos.size() - 1; i++) {
-//            if (pos[i] == pos[i + 1]) {
-//                int dis = distance(alphabet.begin(), find(alphabet.begin(), alphabet.end(),pos[i]));
-//                //cout << "found " << pos[i] << "\n";
-//                solution.push_back(dis);
-//                length -= 2;
-//                if (length == 0) {
-//                    pos.clear();
-//                    break;
-//                }
-//                pos.erase(pos.begin() + i, pos.begin() + i + 2);
-//                i -= 1;
-//            }
-//        }
-//    }
-//    if (pos.empty()) {
-//        cout << "Possible\n";
-//        for (int i : solution) {
-//            cout << i+1 << " ";
-//        }
-//
-//    } else {
-//        cout << "Impossible\n";
-//    }
-//    return 0;
-//}
-
 int main() {
     string input;
     stack<int> cagesIndex;
